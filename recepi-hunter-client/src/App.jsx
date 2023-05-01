7f722c768131e081ec578199a38e39896b0a263f
masudrahman-dev <masudrahman.dev@gmail>
2023-05-02 03:36:35 +0600

    header and footer, requre page and setup done

--- recepi-hunter-client/src/App.jsx
+++ recepi-hunter-client/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/shared/header/Header";
+import Footer from "./components/shared/footer/Footer";
+// import CarouselSlider from "./components/CarouselSlider";
+// import { Footer } from "flowbite-react";
 
 const App = () => {
   return (
-    <div>
-      hello
+    <div className=" flex flex-col min-h-screen ">
+      <Header></Header>
+      {/* <CarouselSlider></CarouselSlider> */}
+  
+      <div className="mt-auto">
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
---
+++ recepi-hunter-client/src/utils/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6A_2NUsNA1UzD5GA9clE8xRDOKWj09jo",
+  authDomain: "recepi-hunter-client.firebaseapp.com",
+  projectId: "recepi-hunter-client",
+  storageBucket: "recepi-hunter-client.appspot.com",
+  messagingSenderId: "780464234426",
+  appId: "1:780464234426:web:7263f1c5b9d92f431a0783",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
